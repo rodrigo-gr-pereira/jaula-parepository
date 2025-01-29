0111452c3e59eb9dbaba9924983e2f3cd72ee161
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2025-01-28 22:16:28 -0300

    Initial commit

---
+++ src/main/java/com/dev/jaula_parepository/JaulaParepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.dev.jaula_parepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaulaParepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaulaParepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/jaula_parepository/JaulaParepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.jaula_parepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaulaParepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
